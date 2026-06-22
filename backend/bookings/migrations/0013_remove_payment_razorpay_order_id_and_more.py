d9810ae6887d7bfee9c8b569cb2baf374659f32f
Annamalai2382 <annamalairaja2002@gmail.com>
2026-06-22 14:19:07 +0530

    Remove unique constraint on gateway_order_id for TiDB compatibility

--- backend/bookings/migrations/0013_remove_payment_razorpay_order_id_and_more.py
+++ backend/bookings/migrations/0013_remove_payment_razorpay_order_id_and_more.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='payment',
             name='gateway_order_id',
-            field=models.CharField(blank=True, max_length=255, null=True, unique=True, verbose_name='Gateway Order ID'),
+            field=models.CharField(blank=True, max_length=255, null=True, unique=False, verbose_name='Gateway Order ID'),
         ),
         migrations.AddField(
             model_name='payment',
